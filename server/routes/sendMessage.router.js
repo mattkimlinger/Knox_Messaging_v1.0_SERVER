00536cb01fdcd0b05b90e9affbc350902606e8e4
Matt Kimlinger <Mkimlinger123@gmail.com>
2020-03-25 19:16:27 -0500

    message uppload complete, app stable

--- server/routes/sendMessage.router.js
+++ server/routes/sendMessage.router.js
@@ -8,41 +8,102 @@ const router = express.Router();
 
 router.use(bodyParser.json());
 
-function getCurrentDate() {
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0');
-    //January is 0!
-    let yyyy = today.getFullYear();
-    today = mm + '/' + dd + '/' + yyyy;
-    console.log('getCurrentDate finished');
-    return today;
-}
-
 router.post("/", async (req, res) => {
     try {
         const body = req.body;
         const token = await body.token;
         // const tokenValidated = tokenValidation(token);
         console.log('tokenValidation(token): ', tokenValidation(token));
-        
+
         const conversationId = body.conversationId;
-        const userId = body.userId;
+        const senderId = body.userId;
         const receiverId = body.receiverId;
         const message = body.message;
-        const date = getCurrentDate();
         console.log('req.body', req.body);
-        // let postMessage = `INSERT INTO messages ( conversation_id, receiver_id, sender_id, message, date_created, time_sent)
-        // VALUES ( $1, $1, $1, $4, $5);`;
-        // pool.query(
-        //     postMessage,
-        //     [conversationId, firstName, lastName, email, password, date]
-        // );
-        res.sendStatus(200);
+        const conversationCheck = `
+            SELECT id 
+            FROM conversations
+            WHERE sender_id = $1
+            AND receiver_id = $2
+            OR receiver_id = $3
+            AND sender_id = $4;
+        `
+        const existingConversationCheck = () => {
+            //Query to check if a conversation already exists
+            return (
+                await pool.query(
+                    conversationCheck,
+                    [
+                        senderId, receiverId,
+                        senderId, receiverId
+                    ]
+                )
+            )
+        }
+        //if no conversations exist between these two users, create one
+        if (!existingConversationCheck()) {
+            try {
+                const newConversation = `
+                    INSERT INTO conversations ( receiver_id, sender_id, last_message)
+                    VALUES ( $1, $2, $3);
+                `;
+                pool.query(
+                    newConversation,
+                    [receiverId, senderId, message]
+                );
+            } catch (error) {
+                console.log('COULD NOT MAKE NEW CONVERSATION error: ', error);
+                res.sendStatus(500)
+            }
+        }
+        //if conversation exist between these two recipients,
+        // update conversation and insert new message
+        if (existingConversationCheck()) {
+            try {
+                const updateConversation = `
+                UPDATE conversations
+                SET receiver_id = $1,
+                sender_id = $3,
+                last_message = $4
+                WHERE sender_id = $5
+                AND receiver_id = $6
+                OR receiver_id = $7
+                ANd sender_id = $8;
+                `;
+                pool.query(
+                    updateConversation,
+                    [
+                        receiverId, senderId, message,
+                        senderId, receiverId, senderId, receiverId
+                    ]
+                );
+                // res.sendStatus(200);
+            } catch (error) {
+                console.log('COULD NOT INSERT MESSAGE error: ', error);
+                return res.sendStatus(500)
+            }
+            try {
+                const postMessage = `
+                    INSERT INTO messages 
+                    ( conversation_id, sender_id, receiver_id, message)
+                    VALUES ( $1, $2, $3, $4 );
+                `;
+                pool.query(
+                    postMessage,
+                    [conversationId, senderId, receiverId, message]
+                );
+                res.sendStatus(200);
+            } catch (error) {
+                console.log('COULD NOT INSERT MESSAGE error: ', error);
+                 return res.sendStatus(500)
+            }
+        }
+
+
     }
     catch (error) {
-    console.log('register error:', error);
-    res.sendStatus(500)
+        console.log('POST New Message Failed: Generic Error:', error);
+        res.sendStatus(500)
     };
 })
 
